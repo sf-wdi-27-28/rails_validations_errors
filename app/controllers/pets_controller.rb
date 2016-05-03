9844a80fc2b03f61716de51325fc48a601db1004
Herman Chan <hermchan89@gmail.com>
2016-05-02 19:11:48 -0700

    created pet index controller, owner specific pets show on pet index page

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,12 @@
 class PetsController < ApplicationController
 
   def index
+    # binding.pry
+    owner_id = params[:owner_id]
+    pet_id = params[:id]
+    @owner = Owner.find_by_id(owner_id)
+    @pet = Pet.find_by_id(pet_id)
+    @pets = @owner.pets
   end
 
   def new
