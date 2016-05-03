39099a9f5f0506107a9dbaa83d0cd58db774ffde
adamrobertreid <WDI_GA@Adams-MacBook-Pro-3.local>
2016-05-02 21:09:18 -0700

    second commit

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,6 +1,13 @@
 class PetsController < ApplicationController
 
   def index
+    owner_id = params[:owner_id]
+
+    @owner = Owner.find_by_id(owner_id)
+
+    @pets = @owner.pets
+
+    render :index
   end
 
   def new
@@ -10,6 +17,13 @@ class PetsController < ApplicationController
   end
 
   def show
+    pet_id = params[:id]
+
+    @pet = Pet.find_by(id: pet_id)
+
+    owner_id = parqams[:owner_id]
+
+    @owner = Owner.find_by(id: owner_id)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root "site#index"
 
   resources :owners do
-    resources :pets
-  end
+  resources :pets
+end
 
 end
