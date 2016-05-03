42f867e585b7a746f0f7647126a952cfeb25e92d
Jonathan <jortiz@tunein.com>
2016-05-03 09:10:04 -0700

    first run end

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,11 +1,32 @@
 class Owner < ActiveRecord::Base
   # TODO: add validations
+  has_many :pets, dependent: :destroy
 
   before_save :normalize_phone_number
+
+  validates :first_name,
+            presence: true,
+            length: {maximum: 255}
+
+  validates :last_name,
+            presence: true,
+            length: {maximum: 255}
+
+  validates :email,
+            presence: true,
+            uniqueness: true,
+            length: {maximum: 255},
+            format: {with: /[@]/, message: "invalid email"}
+
+
 
   # removes leading 1 and the characters (, ), -, .
   def normalize_phone_number
     # stretch
+    if phone.present?
+      phone.gsub!(/^1/, "")
+      phone.gsub!(/[()-.]/,"")
+    end
   end
 
 end
