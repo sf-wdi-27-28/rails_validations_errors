83deb20645154768ad404bb2e7272b8f1362f1d3
altonelli <altonelli14@gmail.com>
2016-05-02 17:56:41 -0700

    finished first sprint

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,4 +1,5 @@
 class OwnersController < ApplicationController
+  before_filter :set_owner, except: [:index,:new,:create]
 
   def index
     @owners = Owner.all
@@ -9,31 +10,53 @@ class OwnersController < ApplicationController
   end
 
   def create
-    owner = Owner.create(owner_params)
-    redirect_to owner_path(owner)
+    owner = Owner.new(owner_params)
+    if owner.save
+      flash[:notice] = "New owner created"
+      redirect_to owner_path(owner)
+    else
+      flash[:error] = owner.errors.full_messages.join(", ")
+      redirect_to new_owner_path
+    end
   end
 
   def show
-    owner_id = params[:id]
-    @owner = Owner.find_by(id: owner_id)
   end
 
   def edit
     # stretch
+
   end
 
   def update
     # stretch
+    if @owner.update(owner_params)
+      flash[:notice] = "New owner Edited"
+      redirect_to owner_path(@owner)
+    else
+      flash[:error] = @owner.errors.full_messages.join(", ")
+      redirect_to edit_owner_path
+    end
   end
 
   def destroy
     # stretch
+    if @owner.destroy
+      flash[:notice] = "New owner Edited"
+      redirect_to owners_path
+    else
+      flash[:error] = @owner.errors.full_messages.join(", ")
+      redirect_to owner_path(@owner)
+    end
   end
 
 
   private
   def owner_params
     params.require(:owner).permit(:first_name, :last_name, :email, :phone)
+  end
+  def set_owner
+    @owner = Owner.find_by("id": params[:id])
   end
 
 end
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,11 +1,17 @@
 class Owner < ActiveRecord::Base
   # TODO: add validations
-
+  validates :first_name, presence: true, length: { maximum: 255 }
+  validates :last_name, presence: true, length: { maximum: 255 }
+  validates :email, presence: true, uniqueness: true, length: { maximum: 255 }, format: { with: /[^@\s]+@[^@\s]+/ }
   before_save :normalize_phone_number
 
   # removes leading 1 and the characters (, ), -, .
   def normalize_phone_number
     # stretch
+    if !self.phone.nil?
+      self.phone = self.phone.gsub(/\)|\(|-|\.|,/,"")
+      self.phone = self.phone.gsub(/1\d{10}/,self.phone[1..self.phone.length])
+    end
   end
 
 end
