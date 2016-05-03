8d5b8fef674cf800211b71b386710aa1df8edb49
Herman Chan <hermchan89@gmail.com>
2016-05-02 17:16:42 -0700

    added foreign keys to pets and owner, pet id matches with owner id, added pet model validation

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,5 +1,10 @@
 class Owner < ActiveRecord::Base
   # TODO: add validations
+  has_many :pets
+
+  validates :first_name, presence: true, length: { maximum: 255 }
+  validates :last_name, presence: true, length: { maximum: 255}
+  validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: true }, format: { with: /[@]/, message: "must include @ character" }
 
   before_save :normalize_phone_number
 
